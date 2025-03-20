6ed76fc6e63535f5e8f663abb4493925761846ac
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-03-20 23:57:56 +0700

    menambahkan StatefulWidget FormPage

---
+++ lib/form_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+
+class FormPage extends StatefulWidget {
+  const FormPage({super.key});
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
