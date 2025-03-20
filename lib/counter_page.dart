e5e5dbed8b4241cc38d7cfb805be73e7b17049b1
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-03-21 00:21:05 +0700

    menambahkan StatefulWidget CounterPage

---
+++ lib/counter_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
